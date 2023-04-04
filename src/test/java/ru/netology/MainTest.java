40a9065420b50772afef0268547a4731bdeaa016
Olia Brizgina <olvitbriz@gmail.com>
2023-04-04 21:14:37 +0400

    initial

---
+++ src/test/java/ru/netology/MainTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void shouldTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+}
