0b0f9f7d282c72fc9e41658ca545b26dff8ff191
Olia Brizgina <olvitbriz@gmail.com>
2023-04-06 20:10:32 +0400

    build failed

--- src/test/java/ru/netology/MainTest.java
+++ src/test/java/ru/netology/MainTest.java
@@ -19,7 +19,7 @@ class MainTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some "));
     }
 
 }
